b4a80123b1e74404df3377238dc964f54766b178
samirjabbar7 <samir.el2001@gmail.com>
2020-12-22 00:37:16 +0100

    here you find ex3

--- TP1/ex03/lu.c
+++ TP1/ex03/lu.c
@@ -7,7 +7,44 @@ float *lu(float A[N][N], float B[N], int n)
 
   /******Implement the solution here******/
   /***************************************/
-
+  float   *x;
+  x = malloc(sizeof(float) * n);
+  float   L[n][n];
+  for(int i=0;i<n;i++)
+	  L[i][i]=1;
+  for(int k=0;k<n-1;k++)
+  {
+    for(int i=k+1;i<n;i++)
+    {
+      L[i][k]=A[i][k]/A[k][k];
+    	for(int j=k+1;j<n;j++)
+      		A[i][j]=A[i][j] - L[i][k]*A[k][j];
+	}
+  }
+  /*descente*/
+  float y[n];
+  //y = malloc(sizeof(float) * n);
+  y[0]=B[0];
+  for(int i=1;i<n;i++)
+  {
+	float s=0;
+	for(int j=0;j<i;j++)
+	{
+		s=s+L[i][j]*x[j];
+	}
+	y[i]=B[i]-s;
+  }
+/*remonte*/
+ 	x[n-1]=y[n-1]/A[n-1][n-1];
+	for(int i=n-2;i>=0;i--)
+	{
+		int s=0;
+		for(int j=i+1;j<n;j++)
+		{
+			s=s+A[i][j]*x[j];
+		}
+	x[i]=(y[i]-s)/A[i][i];
+	}
 
   /***************************************/
   return (x);
